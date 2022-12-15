61861e7a3d31ff52fe8bde8270461204b6dc66f8
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-15 17:23:38 +0300

    wip

--- src/app/components/InventoryGrid/InventoryGrid.vue
+++ src/app/components/InventoryGrid/InventoryGrid.vue
@@ -2,7 +2,11 @@
   <div :class="$style.root">
     <div v-for="row in 5"
          :key="row"
-         :class="$style.row"/>
+         :class="$style.row">
+      <div v-for="column in 5"
+           :key="column"
+           :class="$style.column"/>
+    </div>
   </div>
 </template>
 
@@ -18,14 +22,20 @@
   height: 100vh;
   grid-template-rows: repeat(5, 20%);
   border-radius: 12px;
+  background-color: #4D4D4D;
   box-sizing: border-box;
   overflow: hidden;
+  gap: 1px;
   border: 1px solid #4D4D4D;
 }
 
 .row {
   display: grid;
-  background-color: $main-color;
+  gap: 1px;
   grid-template-columns: repeat(5, 1fr);
+}
+
+.column {
+  background-color: $main-color;
 }
 </style>
